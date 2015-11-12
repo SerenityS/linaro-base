f7070698c9c3506af9432d57f4bdc640d2520389
DQ <dqstyle@gmail.com>
2015-11-12 09:22:58 +0900

    sched: Remove unused parameter from find_new_ilb() The 'call_cpu' is never used in the function. Remove it.
    Reviewed-by: Preeti U Murthy <preeti@linux.vnet.ibm.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1389008085-9069-5-git-send-email-daniel.lezcano@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 3dd0337d6df7d54c82ecebfa6485040f686bf8b1)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6840,7 +6840,7 @@ static int nohz_test_cpu(int cpu)
 }
 #endif
 
-static inline int find_new_ilb(int call_cpu)
+static inline int find_new_ilb(void)
 {
 	int ilb = cpumask_first(nohz.idle_cpus_mask);
 #ifdef CONFIG_SCHED_HMP
@@ -6865,7 +6865,7 @@ static void nohz_balancer_kick(int cpu)
 
 	nohz.next_balance++;
 
-	ilb_cpu = find_new_ilb(cpu);
+	ilb_cpu = find_new_ilb();
 
 	if (ilb_cpu >= nr_cpu_ids)
 		return;
